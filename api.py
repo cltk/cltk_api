4c869d86a5f83502456824549a0220addcd6661f
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-11-19 22:31:27 -0500

    add texts routing

--- api.py
+++ api.py
@@ -8,26 +8,40 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-
+# example
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
+# example
 todos = {}
 class TodoSimple(Resource):
     def get(self, todo_id):
-        return {'asdasd': 'asdasd'}
+        return {'example with token': todo_id}
 
-perseus_latin_authors = []
+
 class Authors(Resource):
-    def get(self):
-        p = os.path.expanduser('~/cltk_data/latin/text/latin_text_perseus/')
-        _list = os.listdir(p)
-        _list.remove('README.md')
-        _list.remove('.git')
-        return {'authors': _list}
+    def get(self, lang):
+        #assert lang in ['greek', 'latin']
+        text_path = os.path.expanduser('~/cltk_data/' + lang + '/text/' + lang + '_text_perseus/')
+        dir_contents = os.listdir(text_path)
+        remove_files = ['README.md', '.git', 'LICENSE.md', 'perseus_compiler.py', '.DS_Store']
+        dir_contents = [f for f in dir_contents if f not in remove_files]
+        return {'authors': sorted(dir_contents)}
+
+
+class Texts(Resource):
+    def get(self, lang, author_name):
+        text_path = os.path.expanduser('~/cltk_data/' + lang + '/text/' + lang + '_text_perseus/' + author_name.casefold() + '/opensource')  # casefold() prob not nec
+        dir_contents = os.listdir(text_path)
+        dir_contents = [f for f in dir_contents if f.endswith('_lat.xml')]
+        dir_contents = [f.casefold() for f in dir_contents]  # this probably isn't nec
+        return {'texts': sorted(dir_contents)}
+
 
 api.add_resource(Authors, '/<string:lang>/authors')
+api.add_resource(Texts, '/<string:lang>/author/<string:author_name>/texts')
+#api.add_resource(Text, '/<string:lang>/author/<string:author_name>/text/<aeneid>')  # Luke is doing
 api.add_resource(TodoSimple, '/todo/<string:todo_id>')
 api.add_resource(HelloWorld, '/')
 
