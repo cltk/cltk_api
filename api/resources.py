b229d634bcdb555c1770cf74f76eef782a62885e
Luke Hollis <lukehollis@gmail.com>
2015-11-22 22:51:54 -0500

    fix query resource to db

--- api/resources.py
+++ api/resources.py
@@ -4,6 +4,7 @@ import pdb
 import json
 from flask import abort, request
 from flask_restful import Resource, Api
+from bson.json_util import dumps
 from util.db import mongo
 from util.jsonp import jsonp
 
@@ -20,7 +21,7 @@ class Query(Resource):
         if 'collection' in params:
             # Save the request collection for later
             request_collection = params.collection
-            del params.collection
+            del params['collection']
 
         else:
             # default collection is text
@@ -30,17 +31,21 @@ class Query(Resource):
         if 'limit' in params:
             # Save the request collection for later
             request_limit = params.limit
-            del params.limit
+            del params['limit']
 
         else:
             # Default limit is 30
             request_limit = 30
 
+        # Remove callback from params
+        if 'callback' in params:
+            del params['callback']
+
         #
         # Very basic query just for testing!
         # Need to sanitize input query params!
         #
-        data = [x for x in db[ request_collection ].find( params )]
+        data = [ x for x in db[ request_collection ].find( params, { '_id' : 0 } ).limit( request_limit) ]
 
 
         return data
--- util/jsonp.py
+++ util/jsonp.py
@@ -4,14 +4,18 @@ from flask import request, current_app
 
 def jsonp(func):
     """Wraps JSONified output for JSONP requests."""
+
     @wraps(func)
     def decorated_function(*args, **kwargs):
+
         callback = request.args.get('callback', False)
         if callback:
             data = str(func(*args, **kwargs))
             content = str(callback) + '(' + data + ')'
             mimetype = 'application/javascript'
             return current_app.response_class(content, mimetype=mimetype)
+
         else:
             return func(*args, **kwargs)
+
     return decorated_function
