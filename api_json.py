44fd9a9e6ef60709498cdfce2c8cd47496102c53
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-11-25 23:20:11 -0800

    add endswith() to file check

--- api_json.py
+++ api_json.py
@@ -39,9 +39,12 @@ class Text(Resource):
         _dir = os.path.join('json', lang, corpus_name)
         files = os.listdir(_dir)
         for file in files:
-            if file.startswith(author):
+            if file.startswith(author) and file.endswith(work + '.json'):
                 file_author, file_work = file.split('__')
 
+                #print(file_work[:-5] == work)
+                #print(file_work[:-5])
+                #print(work)
                 if file_author == author and file_work[:-5] == work:
                     json_fp = os.path.join(_dir, file)
 
