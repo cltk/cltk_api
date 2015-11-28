5d5bdd08bb2e19cec915ce9a8a9afa53a4310214
Kyle P. Johnson <kyle@kyle-p-johnson.com>
2015-11-28 11:33:54 -0800

    return langs, corpora, authors

--- api_json.py
+++ api_json.py
@@ -34,9 +34,9 @@ def open_json(fp):
 
 
 class Text(Resource):
-    def get(self, lang, corpus_name, author, work):
+    def get(self, lang, corpus, author, work):
 
-        _dir = os.path.join('json', lang, corpus_name)
+        _dir = os.path.join('json', lang, corpus)
         files = os.listdir(_dir)
         for file in files:
             if file.startswith(author) and file.endswith(work + '.json'):
@@ -61,20 +61,58 @@ class Text(Resource):
                 if chunk3:
                     text = text[chunk3]
 
-                return{'language': lang,
-                       'corpus_name': corpus_name,
-                       'author': author,
-                       'work': work,
-                       'text': text,
-                       'meta': file_dict['meta'],
-                       }
+                return {'language': lang,
+                        'corpus': corpus,
+                        'author': author,
+                        'work': work,
+                        'text': text,
+                        'meta': file_dict['meta'],
+                        }
+
+
+class Lang(Resource):
+    def get(self):
+        _dir = os.path.join('json')
+        dirs = os.listdir(_dir)
+        return {'languages': dirs}
+
+
+class Corpus(Resource):
+    def get(self, lang):
+        _dir = os.path.join('json', lang)
+        dirs = os.listdir(_dir)
+        return {'language': lang,
+                'corpora': dirs}
+
+
+class Author(Resource):
+    def get(self, lang, corpus):
+        _dir = os.path.join('json', lang, corpus)
+        dirs = os.listdir(_dir)
+        authors_raw = [f[:-5] for f in dirs if f.endswith('.json')]
+        authors = {x.split('__')[0] : x.split('__')[1] for x in authors_raw }
+        return {'language': lang,
+                'authors': authors}
+
+
+
+# http://localhost:5000/lang/latin/corpus/perseus/author
+api.add_resource(Author, '/lang/<string:lang>/corpus/<string:corpus>/author')
+
+
+# http://localhost:5000/lang/latin/corpus
+api.add_resource(Corpus, '/lang/<string:lang>/corpus')
+
+
+# http://localhost:5000/lang/
+api.add_resource(Lang, '/lang')
 
 
 # http://localhost:5000/lang/greek/corpus/perseus/author/achilles_tatius/text/leucippe_et_clitophon?chunk1=1&chunk2=1&chunk3=1
 # http://localhost:5000/lang/greek/corpus/perseus/author/homer/text/odyssey
 # http://localhost:5000/lang/greek/corpus/perseus/author/homer/text/odyssey?chunk1=1&chunk2=1
-api.add_resource(Text, '/lang/<string:lang>/corpus/<string:corpus_name>/author/<string:author>/text/<string:work>')
-#api.add_resource(Text, '/lang/<string:lang>/corpus/<string:corpus_name>/author/<string:author>/text/<string:work>/<string:chunk1>')
+api.add_resource(Text, '/lang/<string:lang>/corpus/<string:corpus>/author/<string:author>/text/<string:work>')
+#api.add_resource(Text, '/lang/<string:lang>/corpus/<string:corpus>/author/<string:author>/text/<string:work>/<string:chunk1>')
 
 # simple examples
 api.add_resource(TodoSimple, '/todo/<string:todo_id>')
